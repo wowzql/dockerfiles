0cbd41a3405b6af992dee7791bd65ee02b09cfa0
Pony <pony@ponycool.com>
2019-04-11 10:46:40 +0800

    Update build.sh

--- etcd/etcd-3.3.10/build.sh
+++ etcd/etcd-3.3.10/build.sh
@@ -6,11 +6,11 @@ docker ps -a | grep "Exited" | awk '{print $2}' | xargs docker rm
 docker images | grep none | awk '{print $3}' | xargs docker rmi
 
 #清除已有的
-docker stop etcd
-docker rm etcd
-docker rmi ponycool/etcd
+docker stop etcd-3.3.10
+docker rm etcd-3.3.10
+docker rmi ponycool/etcd-3.3.10
 
 docker rm $(docker ps -a -q)
 
 #重新生成
-docker build -t ponycool/etcd ./
+docker build -t ponycool/etcd-3.3.10 ./
