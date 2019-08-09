258590b9e444065ab09c1d9d5974e5f88ff9ba56
Pony <pony@ponycool.com>
2019-08-09 11:06:11 +0800

    php 5.6 开发环境docker镜像

---
+++ php-dev/5.6/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#清除tag为none的镜像
+docker ps -a | grep "Exited" | awk '{print $1}' | xargs docker stop
+docker ps -a | grep "Exited" | awk '{print $2}' | xargs docker rm 
+docker images | grep none | awk '{print $3}' | xargs docker rmi
+
+#清除已有的
+docker stop php-dev-5.6
+docker rm php-dev-5.6
+docker rmi ponycool/php-dev-5.6
+
+docker rm $(docker ps -a -q)
+
+#编译生成
+docker build -t ponycool/php-dev-5.6 ./
