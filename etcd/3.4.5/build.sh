e5ee567733ed5c185c183c5136ec116995b84023
Pony <pony@ponycool.com>
2020-03-26 10:19:44 +0800

    etcd dockerfile
    upgrade etcd v3.4.5

---
+++ etcd/3.4.5/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE=etcd
+VERSION=3.4.5
+
+#清除tag为none的镜像
+docker ps -a | grep "Exited" | awk '{print $1}' | xargs docker stop
+docker ps -a | grep "Exited" | awk '{print $2}' | xargs docker rm 
+docker images | grep none | awk '{print $3}' | xargs docker rmi
+
+#清除已有的
+docker stop $IMAGE-$VERSION
+docker rm $IMAGE-$VERSION
+docker rmi ponycool/$IMAGE-$VERSION
+
+docker rm $(docker ps -a -q)
+
+#重新生成
+docker build -t ponycool/$IMAGE-$VERSION ./
