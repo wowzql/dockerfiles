49060f36a061b73d6aa36a5a4208b4f92515c66e
Pony <pony@ponycool.com>
2020-05-30 11:10:23 +0800

    add alpine 3.11

---
+++ alpine/3.11/README.md
@@ -0,0 +1,23 @@
+# Alpine 3.11 Docker 镜像
+
+**日志**
+
+- 1.0
+  - 更换软件安装源为国内阿里云镜像源
+  - 修改时区为Asia/Shanghai
+
+**构建**
+
+```
+./build.sh
+```
+
+**Links**
+
+Blog: http://blog.ponycool.com
+
+Email: pony#ponycool.com(将#替换为@)
+
+Git: https://github.com/PonyCool
+
+DockerHub：https://hub.docker.com/u/ponycool
---
+++ alpine/3.11/build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+IMAGE=alpine
+VERSION=3.11
+
+#清除tag为none的镜像
+docker ps -a | grep "Exited" | awk '{print $1}' | xargs docker stop
+docker ps -a | grep "Exited" | awk '{print $2}' | xargs docker rm 
+docker images | grep none | awk '{print $3}' | xargs docker rmi
+
+#清除已有的
+docker stop $IMAGE-$VERSION
+docker rm $IMAGE-$VERSION
+docker rmi ponycool/$IMAGE-$VERSION
+
+#重新生成
+docker build -t ponycool/$IMAGE-$VERSION ./
